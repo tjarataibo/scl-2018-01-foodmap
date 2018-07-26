126d421ea43cd6e70f4e2f2e88012de9f5fc565b
Tamara <tjarataibo@gmail.com>
2018-07-25 21:15:14 -0400

    Splash, estructura de página hhtml y mapa

---
+++ mapa/mapa.js
@@ -0,0 +1,5 @@
+// Create a script tag and set the USGS URL as the source.
+var script = document.createElement('script');
+
+script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+document.getElementsByTagName('head')[0].appendChild(script);
---
+++ principal/principal.js
@@ -0,0 +1,24 @@
+//Funcionalidad de filtro
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+	start: [20, 80],
+	connect: true,
+	range: {
+		'min': 0,
+		'max': 100
+	}
+});
+
+//Funcionalidad de barra de búsqueda
+$(document).ready(function(){
+    $('.nav-wrapper').nav-wrapper();
+  });
+
+//Funcionalidad para modal
+var instance = M.Modal.getInstance(elem);
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+         
