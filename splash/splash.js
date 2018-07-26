f4eb653b14c7e2f49912363a9894de28ea92e011
Tamara <tjarataibo@gmail.com>
2018-07-26 12:50:00 -0400

    Splash lindo

--- splash/splash.js
+++ splash/splash.js
@@ -1,2 +1,22 @@
-
+(function(){
+ 
+    var preload = document.getElementById("splash");
+    var loading = 0;
+    var id = setInterval(frame, 64);
+   
+    function frame(){
+     if(loading == 100) {
+      clearInterval(id);
+      window.open("../principal/principal.html", "_self");
+     }
+     else {
+      loading = loading + 1;
+      if(loading == 90) {
+       preload.style.animation = "fadeout 1s ease";
+      }
+     }
+    }
+   
+   
+   })();
 
