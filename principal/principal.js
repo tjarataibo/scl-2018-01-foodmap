277b3999aa9c81b7f1b54a87591f6deb8908c27c
Tamara <tjarataibo@gmail.com>
2018-07-26 12:15:12 -0400

    Estructura de la página casi lista, tiene todo en html

--- principal/principal.js
+++ principal/principal.js
@@ -21,4 +21,72 @@
 // $(document).ready(function(){
 //     $('.modal').modal();
 //   });
+
+//Carrousel
+$(document).ready(function(){
+    $('.slider').slider();
+  });
          
+
+  var map;
+  var infowindow;
+  
+  //CurrentPosition = ubicación 
+  navigator.geolocation.getCurrentPosition(initMap);
+  
+  function initMap(position) {
+      //latitud y longitud de geolocalización
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude
+      //Obtener latitud y longitud de Santiago
+      var santiago = {lat, lng};
+    //Predeterminado
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat, lng},
+      zoom: 15
+    });
+    
+    infowindow = new google.maps.InfoWindow();
+     //Variable de PlaceService y enviamos la petición
+     var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      //Rango de lugar, hasta dónde muestra restaurantes
+      location: santiago,
+      radius: 1000,
+      //Búsqueda de restaurant
+      type: ['restaurant']
+    }, callback);
+  
+  
+    function callback(results, status) {
+      
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]); 
+          console.log(results);
+        }
+  
+      }
+    }
+  
+    //Para añadir marcador
+    function createMarker(place) {
+      
+      var loCplace = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+  
+      //Click del marcador
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        
+        infowindow.open(map, this);
+       
+      });
+    }
+      
+    }
+    
+  
