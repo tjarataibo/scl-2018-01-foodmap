ea46b574d1b403aeb658ea7ddb3507cfe8741ba3
Tamara <tjarataibo@gmail.com>
2018-07-25 22:25:02 -0400

    Latitud y longitud de IF

--- mapa/mapa.js
+++ mapa/mapa.js
@@ -3,3 +3,8 @@ var script = document.createElement('script');
 
 script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
 document.getElementsByTagName('head')[0].appendChild(script);
+
+
+//Funcionalidad mapa
+var map = L.map('map').fitWorld();
+
